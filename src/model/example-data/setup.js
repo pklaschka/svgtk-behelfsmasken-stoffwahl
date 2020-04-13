a3fe79e3cb1ce52d1922e27f068cc836f4eaced8
Pablo Klaschka <contact@pabloklaschka.de>
2020-04-13 16:59:25 +0200

    Some more changes

--- src/model/example-data/setup.js
+++ src/model/example-data/setup.js
@@ -4,10 +4,12 @@ const imageData = fs.readFileSync(path.join(__dirname, 'image.png'));
 
 module.exports = async (model) => {
     console.log('Setting up example data...');
-    await model.Fabric.create({
-        image: imageData,
-        name: 'Häkelblüten'
-    });
+
+    for (let i = 1; i <= 5; i++)
+        await model.Fabric.create({
+            image: imageData,
+            name: `Häkelblüten, die ${i}te`
+        });
 
     console.log('Example data setup completed.')
 }
