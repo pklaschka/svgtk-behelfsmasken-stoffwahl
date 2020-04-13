b805afc0bd68b52a4adfa7579c9cdbfde6224429
Pablo Klaschka <contact@pabloklaschka.de>
2020-04-13 15:18:32 +0200

    Adjusted views dir path

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ var indexRouter = require('./routes');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '..', 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
