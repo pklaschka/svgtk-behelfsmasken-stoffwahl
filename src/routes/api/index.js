3ef65a70fa5d0d53be73a7e732d3e7cac85b8335
Pablo Klaschka <contact@pabloklaschka.de>
2020-04-14 14:46:38 +0200

    Adjusted APIs

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,23 +1,17 @@
 const fs = require('fs');
-const path = require('path');
 
 const express = require('express');
 const router = express.Router();
-const model = require('@alias/model');
 const bodyParser = require('body-parser')
 
 const raw = fs.readdirSync(__dirname);
 const versions = raw.filter(v => /^v[0-9]+/.test(v));
 
+router.use(require('cors')());
+
 for (const version of versions) {
     router.use('/' + version, require(`./${version}`));
 }
-
-router.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
 router.use(bodyParser.json())
 
